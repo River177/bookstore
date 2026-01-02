0fbd8407a14d64e20c201b41b03989bcc70712f9
River <flym_e@qq.com>
2026-01-02 19:51:25 +0800

    chore(dependencies): add @prisma/client-runtime-utils and update Vite SSR configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vike(), tailwindcss(), vue()],
+  ssr: {
+    external: ["@prisma/client", ".prisma/client"],
+  },
 });
